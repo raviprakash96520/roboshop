3f90ccf3361fbede05b809f3ea3fdafb84ee1393
raviprakash96520 <raviprakash96520@gmail.com>
2025-11-05 17:10:14 +0530

    Use dynamic path for package.json in pipeline

--- jenkins-shared-library/vars/nodeJSEKSPipeline.groovy
+++ jenkins-shared-library/vars/nodeJSEKSPipeline.groovy
@@ -18,13 +18,14 @@ def call(Map configMap){
             project = configMap.get("project")
             environment = 'dev'
             component = configMap.get("component")
+            path = 'roboshop-ci/catalogue'
         }
 
         stages {
             stage('Read the version') {
                 steps {
                     script{
-                        def packageJson = readJSON file: 'package.json'
+                        def packageJson = readJSON file: env.path/'package.json'
                         appVersion = packageJson.version
                         echo "App version: ${appVersion}"
                     }
